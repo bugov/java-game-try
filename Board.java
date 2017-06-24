486007a7b5d00c73e3f239e09796a655a286f885
Георгий Бажуков <Gosha@MacBook-Pro-Georgij.local>
2017-06-25 03:09:02 +0500

    add timer

--- Board.java
+++ Board.java
@@ -1,6 +1,23 @@
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Board extends JPanel {
-	public Board() {}
+
+public class Board extends JPanel implements ActionListener {
+	private final int TICK = 100;
+	private Timer timer;
+	private int tick_counter = 0;
+
+	public Board() {
+		timer = new Timer(TICK, this);
+		timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		this.tick_counter += 1;
+		System.out.println(this.tick_counter);
+	}
 }
 
